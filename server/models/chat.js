0056a2491d7e0c224bfe87f29ad155fb9abc1674
Joshua Philips <jmichaelphilips@gmail.com>
2016-09-28 13:13:23 -0500

    Updated the mongoose models

--- server/models/chat.js
+++ server/models/chat.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 module.exports = mongoose.model("Chat", {
     "users_id": [String],
+    "userNames": [String],
     "message": [{
         "author": String,
         "message": String, 
